026a2da269dd55609ba933925bd6dc6c9a8c5f49
Denilson Sá <denilsonsa@gmail.com>
2014-01-16 20:24:14 -0200

    Added verboseness option. Renamed map to maze. Renamed deadend to dead-end or dead_end.

--- labirinto.py
+++ labirinto.py
@@ -5,7 +5,75 @@
 from __future__ import division
 from __future__ import print_function
 
+import argparse
+import textwrap
 from PIL import Image
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description=textwrap.dedent('''
+        Given a rectangular maze as an image, this script will parse the image
+        and solve the maze.
+
+        This script was written by Denilson Sá <denilsonsa@gmail.com> as a
+        challenge to solve a maze that was supplied as a PDF file.
+
+        Preparations before running this script:
+        $ pdfimages -j Desafio-labirinto-Desenvolvedor.pdf foo
+        $ mv foo-002.ppm maze.ppm
+        $ rm -f foo-*.ppm
+        Optional (just to save space):
+        $ convert maze.ppm maze.png
+        '''),
+        epilog=textwrap.dedent('''
+        This script expects a maze picture such as:
+        - It is a rectangular grid.
+        - Empty space is white.
+        - Walls are black.
+        - Walls are always perfectly aligned with the grid.
+        - Walls are 1 pixel thick.
+        - Each wall is a few pixels away from each other (i.e. each cell is a
+          few pixels wide).
+        - A cell is considered special (i.e. the start/finish) if the middle
+          pixels are neither white nor black. The color must be saturated
+          enough to survive the preprocessing phase. Colors such as #FF0000 are
+          good choices.
+        '''),
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        '-i', '--save-intermediate',
+        action='store_true',
+        help='''
+        save intermediate images as 01-preprocessed.png and 02-autocropped.png
+        '''
+    )
+    parser.add_argument(
+        '-v', '--verboseness',
+        action='store',
+        type=int,
+        choices=[0, 1, 2, 3],
+        # 0: Only the solution
+        # 1: + the input maze
+        # 2: + the number of dead-ends removed
+        #    + the message 'does not contain cycles'
+        # 3: + the maze after each dead-end removal iteration
+        #    + the maze after removing the cycles.
+        default=1,
+        help='''
+        control how much information will be printed at the output
+        '''
+    )
+    parser.add_argument(
+        'imgfile',
+        action='store',
+        type=argparse.FileType('rb'),
+        help='the picture of the maze'
+    )
+
+    args = parser.parse_args()
+    return args
 
 
 def pixel_preprocessing(pix):
@@ -91,8 +159,8 @@ def find_walls(img):
     # For the proposed input image, non-wall lines have at most 14% black
     # pixels, while wall lines have at least 49%. Thus, the 33% threshold seems
     # reasonable. Although this wall-detection code works fine for randomly
-    # generated labyrinths, it is still possible to craft a labyrinth that will
-    # break this logic.
+    # generated mazes, it is still possible to craft a mazes that will break
+    # this logic.
 
     threshold = width/3.0
     wall_rows = [i for i, amount in enumerate(blacks_per_line)
@@ -148,20 +216,20 @@ class Cell(object):
         self.special = False
 
     @staticmethod
-    def map_as_unicode(map):
+    def maze_as_unicode(maze):
         '''Receives a list of list of Cells and returns a unicode string.'''
         return u'\n'.join(
             u''.join(unicode(cell) for cell in line)
-            for line in map)
+            for line in maze)
 
 
-def build_map_from_image(img):
+def build_maze_from_image(img):
     wall_rows, wall_cols = find_walls(img)
 
     # Measured in cells.
     width = len(wall_cols) - 1
     height = len(wall_rows) - 1
-    map = [[Cell() for i in range(width)] for j in range(height)]
+    maze = [[Cell() for i in range(width)] for j in range(height)]
 
     for i in range(width):
         for j in range(height):
@@ -173,44 +241,55 @@ def build_map_from_image(img):
             # Looking at the middle pixel of the wall. If it is black, there is
             # a wall there.
             if img.getpixel(((x+xn)//2, y)) == (0, 0, 0):
-                map[j][i].up = False
+                maze[j][i].up = False
             if img.getpixel(((x+xn)//2, yn)) == (0, 0, 0):
-                map[j][i].down = False
+                maze[j][i].down = False
             if img.getpixel((x, (y+yn)//2)) == (0, 0, 0):
-                map[j][i].left = False
+                maze[j][i].left = False
             if img.getpixel((xn, (y+yn)//2)) == (0, 0, 0):
-                map[j][i].right = False
+                maze[j][i].right = False
 
             # Looking at the middle pixel of the cell. If it is neither white
             # nor black, it is special.
             if img.getpixel(((x+xn)//2, (y+yn)//2)) not in [
                     (0, 0, 0), (255, 255, 255)]:
-                map[j][i].special = True
+                maze[j][i].special = True
 
-    return map
+    return maze
 
 
-def cut_deadends(map):
-    '''Receives a list of list of Cells, find deadends and remove them.'''
+def cut_dead_ends(maze, verboseness=0):
+    '''Receives a list of list of Cells, find dead-ends and remove them.
 
-    height = len(map)
-    width = len(map[0])
+    Verboseness can be:
+    0: Nothing is printed.
+    1: The number of dead-ends found on each passage is printed.
+    2: The number of dead-ends and the maze are printed.
+    '''
 
-    deadends = []
+    height = len(maze)
+    width = len(maze[0])
 
+    # List of (x, y) coordinates for each dead-end.
+    dead_ends = []
     for i in range(width):
         for j in range(height):
-            if map[j][i].exits == 1:
-                deadends.append((i, j))
+            if maze[j][i].exits == 1:
+                dead_ends.append((i, j))
 
-    while deadends:
-        print('Found {0} deadends.'.format(len(deadends)))
+    num_dead_ends_over_time = []
 
-        old_deadends = deadends
-        deadends = []
+    while dead_ends:
+        if verboseness == 1:
+            num_dead_ends_over_time.append(len(dead_ends))
+        elif verboseness == 2:
+            print('Cutting {0} dead-ends.'.format(len(dead_ends)))
 
-        for x, y in old_deadends:
-            cell = map[y][x]
+        old_dead_ends = dead_ends
+        dead_ends = []
+
+        for x, y in old_dead_ends:
+            cell = maze[y][x]
             directions = [
                 ('up', 'down', 0, -1),
                 ('down', 'up', 0, +1),
@@ -223,38 +302,22 @@ def cut_deadends(map):
                     x2 = x + xdelta
                     y2 = y + ydelta
                     if 0 <= x2 < width and 0 <= y2 < height:
-                        other_cell = map[y2][x2]
+                        other_cell = maze[y2][x2]
                         setattr(other_cell, revdir, False)
                         if other_cell.exits == 1:
-                            deadends.append((x2, y2))
+                            dead_ends.append((x2, y2))
 
-        #print(Cell.map_as_unicode(map))
+        if verboseness == 2:
+            print(Cell.maze_as_unicode(maze))
+
+    if verboseness == 1 and num_dead_ends_over_time:
+        print('Cutting dead-ends: {0}'.format(
+            ', '.join(num_dead_ends_over_time)))
 
 
 def main():
-    # TODO: Receive the filename from the command line.
-    # TODO: Make the comment below a --help output.
-    #
-    # Preparations before running this script:
-    # $ pdfimages -j Desafio-labirinto-Desenvolvedor.pdf foo
-    # $ mv foo-002.ppm labirinto.ppm
-    # $ rm -f foo-*.ppm
-    # Optional (just to save space):
-    # $ convert labirinto.ppm labirinto.png
-    #
-    # This script expects a labyrinth picture such as:
-    # - It is a rectangular grid.
-    # - Walls are black.
-    # - Empty space is white.
-    # - Walls are always perfectly aligned with the grid
-    # - Walls are 1 pixel thick.
-    # - Each wall is a few pixels away from each other (i.e. each cell is a few
-    #   pixels wide).
-    # - A cell is considered special (i.e. the start/finish) if the middle
-    #   pixels are neither white nor black. The color must be saturated enough
-    #   to survive the preprocessing phase. The #FF0000 red is a good choice.
-
-    img = Image.open('labirinto.png')
+    options = parse_arguments()
+    img = Image.open(options.imgfile)
 
     # This script expects RGB images. Let's convert it.
     img = img.convert('RGB')
@@ -262,35 +325,49 @@ def main():
     # Preprocessing the image, essentially removing JPG artifacts by
     # thresholding, and thus reducing the number of colors.
     img = img.point(pixel_preprocessing)
-    img.save('01-preprocessed.png')
+    if options.save_intermediate:
+        img.save('01-preprocessed.png')
 
     # Auto-cropping the white border.
     width, height = img.size
     top, bottom, left, right = find_white_border(img)
-    print('White border detected: top={0} bottom={1} left={2} '
-          'right={3}'.format(top, bottom, left, right))
+    #print('White border detected: top={0} bottom={1} left={2} '
+    #      'right={3}'.format(top, bottom, left, right))
     img = img.crop((left, top, width - right, height - bottom))
     width, height = img.size
-    img.save('02-autocropped.png')
+    if options.save_intermediate:
+        img.save('02-autocropped.png')
 
-    # Building a map of cells from the image pixels.
-    map = build_map_from_image(img)
-    print(Cell.map_as_unicode(map))
+    # Building a maze of cells from the image pixels.
+    maze = build_maze_from_image(img)
+    if options.verboseness >= 1:
+        print('Raw maze:')
+        print(Cell.maze_as_unicode(maze))
 
-    cut_deadends(map)
+    # Solving the maze.
+    cut_dead_ends(maze, options.verboseness - 1)
 
-    print('Is it over yet?')
-    print(Cell.map_as_unicode(map))
-    print('Now trying to remove the special cells too.')
+    if options.verboseness >= 0:
+        print('Solution:')
+        print(Cell.maze_as_unicode(maze))
 
-    for line in map:
+    # Checking for cycles. After removing the special attribute of the cells
+    # and running the solver again, all cells should have no exits. Otherwise,
+    # there is a cycle.
+    for line in maze:
         for cell in line:
             if cell.special:
                 cell.special = False
+    cut_dead_ends(maze)
 
-    print(Cell.map_as_unicode(map))
-    cut_deadends(map)
-    print(Cell.map_as_unicode(map))
+    if any(cell.exits != 0 for cell in line for line in maze):
+        if options.verboseness >= 0:
+            print('This maze contains cycles.')
+    else:
+        if options.verboseness >= 2:
+            print('This maze does not contain cycles.')
+    if options.verboseness >= 3:
+        print(Cell.maze_as_unicode(maze))
 
 
 if __name__ == '__main__':
